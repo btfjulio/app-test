a44f881a4f4e3e4d27b4ddca99977a5c629f8b21
Bruno Tostes <btfjulio@hotmail.com>
2020-07-18 13:53:29 -0300

    add user sign out testes

--- spec/features/user_sigininout_spec.rb
+++ spec/features/user_sigininout_spec.rb
@@ -7,9 +7,7 @@ RSpec.feature "User Session" do
       email: "john@example.com",
       password: "password"
     )
-  end
 
-  scenario "with valid credentials" do
     visit "/"
 
     click_link "Sign In"
@@ -17,12 +15,16 @@ RSpec.feature "User Session" do
     fill_in "Email", with: @john.email
     fill_in "Password", with: @john.password
     click_button "Log in"
-
-    expect(page).to  have_content("Signed in successfully.")
-    expect(page).to  have_content("Signed in as #{@john.email}") 
-    expect(page).not_to have_link("Sign In")
-    expect(page).to have_link("Sign Out")
   end
 
+  scenario "signing user out" do
+    visit "/"
+
+    click_link "Sign Out"
+
+    expect(page).to  have_content("Signed out successfully")
+    expect(page).to  have_content("Sign In")
+    expect(page).not_to  have_content("Sign Out")
+  end
 
 end
